f751f6d341bc5bf998d2a43c23b2861307065b9b
Bhavin Sen <senbhavin@gmail.com>
2020-05-20 20:30:20 +0530

    cors issue.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\Cors;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'auth'], function (){
+Route::middleware([Cors::class])group(['prefix' => 'auth'], function (){
 
     Route::post('login', 'AuthController@login')->name('login');
     Route::post('signup', 'AuthController@signup');
